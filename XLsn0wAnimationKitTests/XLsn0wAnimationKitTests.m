ed8fc9816f42dd3d31c5f3f2dce564349a6657f3
XLsn0w <XLsn0w@outlook.com>
2018-05-27 13:31:26 +0800

    init

---
+++ XLsn0wAnimationKitTests/XLsn0wAnimationKitTests.m
@@ -0,0 +1,39 @@
+//
+//  XLsn0wAnimationKitTests.m
+//  XLsn0wAnimationKitTests
+//
+//  Created by XLsn0w on 2018/5/27.
+//  Copyright © 2018年 XLsn0w. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XLsn0wAnimationKitTests : XCTestCase
+
+@end
+
+@implementation XLsn0wAnimationKitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
